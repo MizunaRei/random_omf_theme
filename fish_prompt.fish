0058ca024321c6df620d85235c26858396671409
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-10-31 23:04:35 +0900

    make function fish_prompt to be function main

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,8 @@
+function fish_prompt
+## themes in omf could not used by source command
+## source $OMF_PATH/themes/$theme_to_enable/functions/fish_prompt.fish
+
+
 while true
 set theme_to_enable $(omf.index.query --type=theme | shuf -n 1)
 	if test "random" != $theme_to_enable
@@ -9,16 +14,16 @@ end
 
 if not contains   "$theme_to_enable"  $(omf.packages.list --theme)
 		omf install $theme_to_enable
-		printf " \n $theme_to_enable is installed. \n "
+		## printf " \n $theme_to_enable is installed. \n "
 		end
 
 
 omf theme $theme_to_enable
-printf " \n $theme_to_enable is enabled. \n "
+## printf " \n $theme_to_enable is enabled. \n "
 
 
-function fish_prompt
-## themes in omf could not used by source command
-## source $OMF_PATH/themes/$theme_to_enable/functions/fish_prompt.fish
-printf " \n $theme_to_enable is enabled. \n "
+
+
+
+## printf " \n $theme_to_enable is enabled. \n "
 end
