84413089b38aefbc27cee99a20ee20bacbae8019
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-10-31 22:36:18 +0900

    fix error theme is installed

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,20 @@
 while true
-set theme $(omf.index.query --type=theme | shuf -n 1)
-	if test "random" != $theme
+set theme_to_enable $(omf.index.query --type=theme | shuf -n 1)
+	if test "random" != $theme_to_enable
+	and test "random_omf_theme" != $theme_to_enable
+		if not contains   "$theme_to_enable"  $(omf.packages.list --theme)
+		omf install $theme_to_enable
+		end
 	break
 	end
 end
-omf install $theme
-omf theme $theme
+
+
+omf theme $theme_to_enable
 
 
 function fish_prompt
-## theme could not used by source command
-## source $OMF_PATH/themes/$theme/functions/fish_prompt.fish
-printf " $theme is enabled. "
+## themes in omf could not used by source command
+## source $OMF_PATH/themes/$theme_to_enable/functions/fish_prompt.fish
+echo " $theme_to_enable is enabled. "
 end
