55c40df9174ade42d902a264ae1d01dc0756f886
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-10-31 22:00:11 +0900

    update fuction fish_prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,15 @@
 while true
-# outputs 'file exists' at 10 second intervals,
-# as long as the file foo.txt or bar.txt exists.
 set theme $(omf.index.query --type=theme | shuf -n 1)
 	if test "random" != $theme
 	break
 	end
 end
 omf install $theme
+omf theme $theme
+
+
 function fish_prompt
 ## theme could not used by source command
 ## source $OMF_PATH/themes/$theme/functions/fish_prompt.fish
 printf " $theme is enabled. "
-omf theme $theme
 end
