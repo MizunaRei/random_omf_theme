3355cc57f5ba0d15e696a327c37381f46e6de07f
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-11-04 13:33:13 +0900

    formatting

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -10,45 +10,45 @@ function fish_prompt
 ## printf "default" > $OMF_CONFIG/theme
 
 
-## workaround for priority of fish_prompt.fish
- if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
- rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
-end
+## workaround for priority of fish_prompt.fish . It cause omf theme command fail.
+	if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
+	rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
+	end
 
 
- ## pickup a theme_to_enable in omf official repo
-while true
-set theme_to_enable (random choice (omf.index.query --type=theme ))
+## pickup a theme_to_enable from omf official repo
+	while true
+	set theme_to_enable (random choice (omf.index.query --type=theme ))
 	if test "random" != $theme_to_enable
 		if test "random_omf_theme" != $theme_to_enable
 		break
 		end
 	end
-end
+	end
 
 
-if not contains   "$theme_to_enable"  $(omf.packages.list --theme)
-omf.cli.install $theme_to_enable
-## printf \n
+	if not contains   "$theme_to_enable"  $(omf.packages.list --theme)
+	omf.cli.install $theme_to_enable
+## call omf function instead of using shell command
 ## omf install $theme_to_enable
-## printf " \n $theme_to_enable is installed. \n "
+	end
+
+
+## enable new theme
+	omf.cli.theme $theme_to_enable
+	
+	
+## fix prompt disappeared after enabling new theme.  
+	omf.cli.reload
 end
 
 
-omf.cli.theme $theme_to_enable
-## printf " \n $theme_to_enable is enabled. \n "
-## prompt disappeared after enabling new theme. sometimes omf theme command failed.
-## omf theme $theme_to_enable ; and omf reload
-omf.cli.reload
-end
-
-
-## execute this script directly in fish instead of using omf
+## source this script directly in fish instead of using omf theme command to fix file priority issue
 ## chmod +x  $OMF_PATH/themes/random_omf_theme/functions/fish_prompt.fish
 
 
 function omf.theme.remove_all_themes
-rm -rf $OMF_PATH/themes/*
+	rm -rf $OMF_PATH/themes/*
 end
 
 
