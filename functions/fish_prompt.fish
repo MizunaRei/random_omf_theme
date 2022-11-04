966174b0242e406e49baf9ef6307cd088df8d89c
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-11-04 19:49:39 +0900

    enable random theme every startup time

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -6,11 +6,8 @@ function fish_prompt
 ## source $OMF_PATH/themes/$theme_to_enable/functions/fish_prompt.fish
 
 
-## force omf to enable another theme
-## printf "default" > $OMF_CONFIG/theme
-
-
-## workaround for priority of fish_prompt.fish . It cause omf theme command fail.
+## workaround for priority of fish_prompt.fish . It cause omf theme command fail. 
+## omf theme this script file will not call rm command to fix the issue.
 	if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
 	rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
 	end
@@ -36,20 +33,30 @@ function fish_prompt
 
 ## enable new theme
 	omf.cli.theme $theme_to_enable
-	
+
+
+## force omf to enable a new theme when fish source dotfiles (i.e. omf reload)
+	printf "random_omf_theme" > $OMF_CONFIG/theme
+
 	
 ## fix prompt disappeared after enabling new theme.  
 	omf.cli.reload
+	
+	
+## function fish_prompt end	
 end
 
 
-## source this script directly in fish instead of using omf theme command to fix file priority issue
+
+## source this script directly in fish instead of using omf theme command to fix fish_prompt.fish priority issue
 ## chmod +x  $OMF_PATH/themes/random_omf_theme/functions/fish_prompt.fish
 
 
+## holding too many themes and plugins might cause issues.
 function omf.theme.remove_all_themes
 	rm -rf $OMF_PATH/themes/*
 end
 
 
+## source this script file to fix fish_prompt.fish priority issue.
 fish_prompt
