7a4fd6b4dcba65f24da5a11711c791cb56e202d0
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-11-11 14:35:35 +0900

    skip installation for local themes

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # random_omf_theme🐠
 randomly install and enable themes from [official packages repository](https://github.com/oh-my-fish/packages-main) of [oh-my-fish/oh-my-fish](https://github.com/oh-my-fish/oh-my-fish)  for [fish shell](https://fishshell.com/)
-### Installation & Usage🛫 
-Install [oh-my-fish/oh-my-fish](https://github.com/oh-my-fish/oh-my-fish#installation) for the fish shell first. And then execute these commands in the fish shell. 
+### Installation & Usage🛫
+Install [oh-my-fish/oh-my-fish](https://github.com/oh-my-fish/oh-my-fish#installation) for the fish shell first. And then execute these commands in the fish shell.
 ```
 git clone --depth=1 https://github.com/MilkyMAISHIRANUI/random_omf_theme $OMF_PATH/themes/random_omf_theme
 ```
-For a random theme, type this. 
+To enable random themes, type this.
 ```
 omf theme random_omf_theme
 ```
+If random_omf_theme is enabled already, `omf theme random_omf_theme` does nothing. So it does with other themes. Please use above command instead.
+```
+omf reload
+```
 We are not listed in the oh-my-fish packages repository. Therefore you could not use the `omf install random_omf_theme` command.
-### Known Issues ⚠️ 
+### Known Issues ⚠️
  `fish_config` command persists the prompt to `~/.config/fish/functions/fish_prompt.fish` . That file takes precedence over Oh My Fish's themes. Therefore command `omf theme <theme_name>` will not work.  `aight`,`boxfish`,`sashimi`,`slavic-cat` and other themes will cause this issue.
-To fix it as `omf doctor` suggested, enter this command `rm ~/.config/fish/functions/fish_prompt.fish` .
-Or type this command `source $OMF_PATH/themes/random_omf_theme/functions/fish_prompt.fish` and let us do that for you automatically and enable a new random theme.
-
-If random_omf_theme is enabled already, `omf theme random_omf_theme` does nothing. So it does with other themes. Please use `omf reload` command instead.
+To fix it as `omf doctor` suggested, enter this command.
+```
+rm ~/.config/fish/functions/fish_prompt.fish
+```
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -8,22 +8,31 @@
 function fish_prompt
 	## workaround for priority of fish_prompt.fish . It cause omf theme command failing. 
 	## calling omf theme this script file will not call rm command to fix the issue.
-	## if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
-	## rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
-	## end
+	# if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
+	# rm (omf.xdg.config_home)/fish/functions/fish_prompt.fish 
+	# end
 
+
+	## workaround for priority of fish_prompt.fish . It blocks random theme enabling. 
+	if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
+	mv -f (omf.xdg.config_home)/fish/functions/fish_prompt.fish (omf.xdg.config_home)/fish/functions/fish_prompt.fish.old
+	end	
+
+
+	set themes_local_installed "$(omf.packages.list --theme)"
+	set themes_in_omf_repository "$(omf.index.query --type=theme)"
 
 	## pickup a theme_to_enable from omf official repo
 	while true
-		if test -n "$(omf.index.query --type=theme)"
+		if test -n "$themes_in_omf_repository"
 		set theme_to_enable "$(random choice (omf.index.query --type=theme))"
 		else
 		set theme_to_enable "$(random choice (omf.packages.list --theme))"
 		end
 
-		if test -z "$theme_to_enable"
-		set theme_to_enable "$(random choice (omf.packages.list --theme))"
-		end
+		# if test -z "$theme_to_enable"
+		# set theme_to_enable "$(random choice (omf.packages.list --theme))"
+		# end
 
 		if test "random" != "$theme_to_enable"
 			if test "random_omf_theme" != "$theme_to_enable"
@@ -33,42 +42,70 @@ function fish_prompt
 	end
 
 
+	
+switch $themes_local_installed
+case "*$theme_to_enable*" 
+    ## debugging
+    # echo "$theme_to_enable"
+    omf theme "$theme_to_enable"
+    echo  (omf::em)"✔ $theme_to_enable theme successfully enabled."(omf::off) 	
+
+case '*'
+    ## debugging
+    # echo "$themes_local_installed"
+    omf install "$theme_to_enable"
+   omf theme "$theme_to_enable"
+    echo  (omf::em)"✔ $theme_to_enable theme successfully enabled."(omf::off) 	
+
+end
+
+
 	## install remote theme if not available locally
-	if not contains "$theme_to_enable" "$(omf.packages.list --theme)"
-	## omf.cli.install "$theme_to_enable"
+	# omf.cli.install "$theme_to_enable"
 	## use low level functions for performance
 	## use high level functions for compatibility
-	omf install "$theme_to_enable"
-	end
+	# if not contains "$theme_to_enable" "$themes_local_installed"
+	# omf install "$theme_to_enable"
+	# end
 
+	# if test -n (string match -e "$theme_to_enable" "$(omf.packages.list --theme)")
+	# omf install "$theme_to_enable"
+	# end
+
+	
 
         ## enable the new theme
 	## use low level functions for performance
-        ## omf.theme.set "$theme_to_enable"
+        # omf.theme.set "$theme_to_enable"
 	## use high level functions for compatibility
-	omf theme "$theme_to_enable"
+	# omf theme "$theme_to_enable"
 	
 
-	## workaround for priority of fish_prompt.fish . It blocks random theme enabling. 
-	if test -e (omf.xdg.config_home)/fish/functions/fish_prompt.fish
-	mv -f (omf.xdg.config_home)/fish/functions/fish_prompt.fish (omf.xdg.config_home)/fish/functions/fish_prompt.fish.old
-	end
+	## printf does not display text on terminal
+	# echo  (omf::em)"✔ $theme_to_enable theme successfully enabled."(omf::off) 	
 	
 
 	## prompt line disappears after enabling new theme. That may be a bug of fish shell.
 
 
 	## When random theme is enabled, omf reload command will cause dead loop.
-	## omf.cli.reload
+	# omf.cli.reload
 	
 	
 	## prompt line appears without theme after enabling a new theme.
 	## The new theme will be enabled after a command returns.
-	printf " \r "
+	# printf " \r "
 
 
 	## force omf to enable a new theme when fish source dotfiles (i.e. omf reload)
-	printf "random_omf_theme" > "$OMF_CONFIG"/theme
+	echo "random_omf_theme" > "$OMF_CONFIG"/theme
+
+
+	## prompt line disappears after enabling new theme. That may be a bug of fish shell.
+	## The new theme will be enabled after a command returns.
+	## make a fake prompt line for end users.
+	pwd
+	echo '>'
 
 	
 ## function fish_prompt end	
